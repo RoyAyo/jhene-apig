2b76e761c9c47cec8d76fe7f1f219de34bca6fe4
RoyAyo <roylayindeay0@gmail.com>
2020-12-03 07:30:12 +0100

    the irst set of functions successfuly initialized

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+
+class Messages(BaseModel):
+    message : str
+    from_context : str
+    answers : dict
 
 app = FastAPI()
 
@@ -6,3 +12,7 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
+
+@app.post("/send_message")
+def send_message(data:Messages):
+    return data
