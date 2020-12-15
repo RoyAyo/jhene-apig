e27f675971c82014f2e364baead20db1587436f4
RoyAyo <roylayindeay0@gmail.com>
2020-12-15 12:40:08 +0100

    added a new req.txt file

--- utils/bot.py
+++ utils/bot.py
@@ -6,7 +6,7 @@ import random
 import numpy as np
 from tensorflow.keras.models import model_from_json
 from tensorflow.compat.v1 import get_default_graph, Session
-from tensorflow.compat.v1.keras.backend import set_sessio
+from tensorflow.compat.v1.keras.backend import set_session
 lemmatizer = WordNetLemmatizer()
 
 class Evaluate():
