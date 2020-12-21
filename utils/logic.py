ebe4c437450b04d77a9895c25a157b2782950903
RoyAyo <roylayindeay0@gmail.com>
2020-12-21 03:16:43 +0100

    added new workflow and logic to fetching db

--- utils/logic.py
+++ utils/logic.py
@@ -1,23 +1,22 @@
-from utils.bot import Evaluate
+# from utils.bot import Evaluate
 import pymongo
 from pymongo.errors import CollectionInvalid
 from collections import OrderedDict
 
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 
-db = client["jhene"]
+db = client["jhene-db"]
 
-evaluate = Evaluate()
-
-user_schema = {
-
-}
+# evaluate = Evaluate()
 
 class Logic:
     def get_response(self,data):
         message = data.message.strip()
         if(data.more_info):
-
+            answers = data['answers']
+            location = data['location']
+            context = data['from_context']
+            self.search_user(context,answers,location)
         else :
             return self.use_bot(message)
 
@@ -42,7 +41,25 @@ class Logic:
     def get_responses(self):
         return "get response"
 
-    def search_user(self, context):
-        businesses = mydb
+    def search_user(self, context_, answers={}, location = ''):
+        context = context_.split('_')[0]
+        query = {'products' : {'$all' : [context]}}
+        if answers['gender']:
+            gender = 1 if answers['gender'].lower() == 'female' else 2
+            query['owner_details'] = {'gender' : gender}
+        if answers['location'] or answers['location']:
+            #find a way to get just the local govt area from geolocation lat and longs
+            location = answers['location']
+            query['work_locations'] = {'$all' : [location]}
+        customers = db['customers'].find(query)
         #check collection and returns a user that fits at least the profile
         return "user"
+
+
+def search_db():
+    customers = db['customers'].find({'products' : {'$all' : ['shoes']}, 'work_locations':{'$all' : ['Akoka']}, 'owner_details' : {'gender': '1'} })
+    for customer in customers:
+        print(customer)
+
+if __name__ == "__main__":
+    search_db()
