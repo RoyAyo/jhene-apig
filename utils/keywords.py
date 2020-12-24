915a544ffde22bd7a45666e746cfa1cdf67646fa
RoyAyo <roylayindeay0@gmail.com>
2020-12-24 10:00:00 +0100

    kinda initialized some keywords to be used

--- utils/keywords.py
+++ utils/keywords.py
@@ -1,4 +1,4 @@
-const keywords = {
+const props_keywords = {
     'shoe_plug' : {
         'sneakers' : 'sneakers',
         'nike' : 'sneakers',
@@ -11,7 +11,7 @@ const keywords = {
         'yeezy' : 'sneakers'
     },
     'cloth_plug' : {
-        'shirt' : 'shirts',
+        't-shirt' : 't-shirts',
         'top' : 'tops',
         'skirt' : 'skirts',
         'suit' : 'suits',
@@ -30,16 +30,29 @@ const keywords = {
         "biker" : "biker_shorts"
     },
     "watch_plug" : {
-
+        "casio" : "casio",
+        "cartier" : "cartier",
+        "benson" : "benson",
+        "hublot" : "hublot",
+        "millie" : "richard_millie",
+        "bell" : "bell_and_ross",
+        "schaffhausen":"schaffhausen"
     },
     "jersey_plug":{
 
     },
     "Jewelry_plug":{
-
+        "bracelet" : "bracelets",
+        "ring" : "rings",
+        "necklace" : "necklace",
+        "earring" : "earring",
+        "cufflink" : "cufflink"
     },
     "slips_plug":{
-
+        "nike" : "nike",
+        "adidas" : "adidas",
+        "puma" : "puma",
+        "crocs" : "crocs"
     },
     "heels_plug":{
         
@@ -48,10 +61,11 @@ const keywords = {
 
     },
     "interior_decor_plug":{
-
+        "painter" : "painter",
+        "renovator" : "renovator"
     },
     "tailor_plug":{
-
+        
     },
     "phone_plug":{
 
@@ -63,13 +77,18 @@ const keywords = {
 
     },
     "laptop_plug":{
-
+        "macbook" : "macbook",
+        "hp" : "hp",
+        "dell" : "dell",
+        "toshiba" : "toshiba",
+        "hewlett" : "hp"
     },
     "cake_plug":{
-
+        "cupcake" : "cupcakes"
     },
     "eventplanner_plug":{
-
+        "wedding" : "wedding",
+        "birthday" : "birthday"
     },
     "laundryservice_plug":{
 
@@ -78,63 +97,136 @@ const keywords = {
 
     },
     "mua_plug":{
-
+        "products" : "makeup_products"
     },
     "car_plug":{
-
+        "toyota" : "toyota",
+        "benz" : "benz",
+        "rover" : "land_rover",
+        "mercedes" : "benz",
+        "mini-bus" : "bus",
+        "audi" : "audi",
+        "volkswagon" : "volkswagon"
     },
     "monitor_plug":{
-
+        "samsung" : "samsung",
+        "curved" : "curved_monitor"
     },
     "tv_plug":{
-
+        "samsung" : "samsung",
+        "curved" : "curved_tv"
     },
     "furniture_plug":{
-
+        "chair" : "chairs",
+        "cupboard" : "cupboards",
+        "table" : "tables"
     },
     "wig_plug":{
-
+        "bone" : "bone_straight"
     },
     "perfume_plug":{
-
+        "oil" : "perfume_oil"
     },
     "skincareproducts_plug":{
-
+        "moisturizer" : "moisturizer"
     },
-    "handbag_plug":{
-
+    "bag_plug":{
+        "purse" : "purse",
+        "travelling" : "travelling_bag",
+        "hand" : "hand_bag",
+        "school" : "school_bag",
+        "laptop" : "laptop_bag"
     },
     "in_yaba" : {
-
+        "cloth" , "cloth",
+        "belt" : "belt",
+        "shoe" : "shoe",
+        "schoolbag" : "school_bag",
+        "travellingbag" : "travelling_bag",
+        "handbag" : "hand_bag",
+        "purse" : "purse"
     },
     "in_computer_village" : {
-
+        "repair" : "repair",
+        "laptop" : "laptop",
+        "phone" : "phone",
+        "charger" : "charger",
+        "accessory" : "accessory"
     },
     "in_oshodi" : {
-
+        "cloth" , "cloth",
+        "belt" : "belt",
+        "shoe" : "shoe",
+        "schoolbag" : "schoolbag",
+        "travellingbag" : "travellingbag",
+        "handbag" : "handbag",
+        "purse" : "purse"
     },
     "in_eko" : {
-
+        "cloth" , "cloth",
+        "belt" : "belt",
+        "shoe" : "shoe",
+        "schoolbag" : "schoolbag",
+        "travellingbag" : "travellingbag",
+        "handbag" : "handbag",
+        "purse" : "purse"
     },
     "phone_accessory" : {
-
+        "charger" : "charger",
+        "cord" : "cord",
+        "airpod" : "airpod",
+        "earbud" : "earbud"
     },
     "laptop_accessory" : {
-        
+        "charger" : "charger",
+        "mouse" : "mouse",
+        "hdd" : "harddrive",
+        "harddrive" : "harddrive",
+        "ssd" : "harddrive",
+        "ram" : "ram"
     },
     "btc_sales" : {
-        
+        "cashapp" : "cashapp",
+        "btc" : "btc",
+        "bitcoin" : "btc",
+        "eth" : "eth",
+        "etherum" : "eth"
     },
     "gym_equipment" : {
 
     },
     "house_equipment" : {
-
+        "washing" : "washine_machine",
+        "fridge" : "fridge",
+        "freezer" : "fridge",
+        "refridgerator" : "fridge"
     },
     "food" : {
 
     },
     "snack_food" : {
+        "shawarma" : "shawarma",
+        "burger" : "burger",
+        "taco" : "taco",
+        "pasta" : "pasta",
+        "chips" : "chips",
+        "pizza" : "pizza",
+        "chicken" : "chicken"
+    }
+    "baby_product" : {
 
     }
+}
+
+const gender_keyword = {
+    "male" : "male",
+    "female" : "female",
+    "man" : "male",
+    "men" : "male",
+    "woman" : "female",
+    "women" : "female",
+    "womyn" : "female",
+    "boy" : "male",
+    "girl" : "female",
+    "lady" : "female"
 }
