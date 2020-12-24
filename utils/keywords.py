c7b1458bd8b9f0ff772ef3b549cbc520badc9d78
RoyAyo <roylayindeay0@gmail.com>
2020-12-24 07:51:42 +0100

    added new tags to the keywords file

--- utils/keywords.py
+++ utils/keywords.py
@@ -4,19 +4,18 @@ const keywords = {
         'nike' : 'sneakers',
         'adidas' : 'sneakers',
         'puma' : 'sneakers',
-        "ankle" : "ankle_boot",
+        "ankle" : "ankle_boot":{},
         'chelsea' : 'chelsea_boot',
         'loafers' : 'loafers'
-        'boots' : 'boots',
-        'boot' : 'boots'
+        'boot' : 'boots',
         'yeezy' : 'sneakers'
     },
     'cloth_plug' : {
-        'gown' : 'gowns',
-        'suit' : 'suits',
         'shirt' : 'shirts',
         'top' : 'tops',
         'skirt' : 'skirts',
+        'suit' : 'suits',
+        'gown' : 'gowns',
         'pant' : 'pants',
         'palazzo' : 'palazzo',
         'jersey' : 'jersey',
@@ -29,5 +28,113 @@ const keywords = {
         "blouse" : "blouse"
         'alternative' : 'alte_clothes'
         "biker" : "biker_shorts"
-    }    
+    },
+    "watch_plug" : {
+
+    },
+    "jersey_plug":{
+
+    },
+    "Jewelry_plug":{
+
+    },
+    "slips_plug":{
+
+    },
+    "heels_plug":{
+        
+    },
+    "durag_plug":{
+
+    },
+    "interior_decor_plug":{
+
+    },
+    "tailor_plug":{
+
+    },
+    "phone_plug":{
+
+    },
+    "camera_plug":{
+
+    },
+    "photographer_plug":{
+
+    },
+    "laptop_plug":{
+
+    },
+    "cake_plug":{
+
+    },
+    "eventplanner_plug":{
+
+    },
+    "laundryservice_plug":{
+
+    },
+    "realestateagent_plug":{
+
+    },
+    "mua_plug":{
+
+    },
+    "car_plug":{
+
+    },
+    "monitor_plug":{
+
+    },
+    "tv_plug":{
+
+    },
+    "furniture_plug":{
+
+    },
+    "wig_plug":{
+
+    },
+    "perfume_plug":{
+
+    },
+    "skincareproducts_plug":{
+
+    },
+    "handbag_plug":{
+
+    },
+    "in_yaba" : {
+
+    },
+    "in_computer_village" : {
+
+    },
+    "in_oshodi" : {
+
+    },
+    "in_eko" : {
+
+    },
+    "phone_accessory" : {
+
+    },
+    "laptop_accessory" : {
+        
+    },
+    "btc_sales" : {
+        
+    },
+    "gym_equipment" : {
+
+    },
+    "house_equipment" : {
+
+    },
+    "food" : {
+
+    },
+    "snack_food" : {
+
+    }
 }
